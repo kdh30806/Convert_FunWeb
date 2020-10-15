3d80c64553c86969bd6eb4d58ead6448df3476ee
82103 <82103@DESKTOP-6FVH2SO>
2020-10-15 17:06:09 +0900

    2020-10-15

--- funweb/src/main/java/com/funweb/service/MemberServiceImp.java
+++ funweb/src/main/java/com/funweb/service/MemberServiceImp.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.funweb.dao.MemberDAO;
 
 @Repository
-public class MemberServiceImp {
+public class MemberServiceImp implements MemberService{
 
 	@Inject
 	private MemberDAO memberDAO;
