1477f1b90f992f869c9792809403a2b8eb41946b
82103 <82103@DESKTOP-6FVH2SO>
2020-10-26 15:25:09 +0900

    2020-10-26 picture 수정 기능 구현

--- FunWeb_Spring/src/main/java/com/funweb/controller/BoardController.java
+++ FunWeb_Spring/src/main/java/com/funweb/controller/BoardController.java
@@ -23,192 +23,192 @@ import com.funweb.service.BoardService;
 
 @Controller
 public class BoardController {
-	
+
 	@Inject
 	private BoardService boardService;
-	
+
 	@Resource(name = "uploadPath")
 	private String uploadPath;
 
 //==========================================notic=============================================	
-	
+
 	@RequestMapping(value = "/board/notice/main", method = RequestMethod.GET)
-	public String noticeMain(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String noticeMain(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		String pageNum = request.getParameter("pageNum");
 		String category = "notice";
 		PageBean pb = new PageBean();
 		pb.setPageSize(10);
-		if(pageNum == null) {
+		if (pageNum == null) {
 			pageNum = "1";
 		}
 		pb.setPageNum(pageNum);
 		pb.setCategory(category);
 		List<BoardBean> noticeList = boardService.getBoardList(pb);
-		
+
 		pb.setCount(boardService.getBoardCount(category));
-		
+
 		session.setAttribute("id", id);
 		model.addAttribute("noticeList", noticeList);
 		model.addAttribute("pb", pb);
-		
+
 		return "/board/notice/main";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/write", method = RequestMethod.GET)
-	public String noticeWrite(HttpSession session) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String noticeWrite(HttpSession session) {
+
+		String id = (String) session.getAttribute("id");
 		session.setAttribute("id", id);
-		
+
 		return "/board/notice/write";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/write", method = RequestMethod.POST)
-	public String noticeWritePost(BoardBean bb) {	
-		
+	public String noticeWritePost(BoardBean bb) {
+
 		bb.setCategory("notice");
 		boardService.writeBoard(bb);
-		
+
 		return "redirect:/main";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/content", method = RequestMethod.GET)
-	public String noticeContent(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String noticeContent(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("notice");
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/notice/content";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/update", method = RequestMethod.GET)
-	public String noticeUpdate(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String noticeUpdate(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("notice");
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/notice/update";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/update", method = RequestMethod.POST)
-	public String noticeUpdatePost(BoardBean bb) {	
-		
+	public String noticeUpdatePost(BoardBean bb) {
+
 		bb.setCategory("notice");
 		boardService.updateBoard(bb);
-	
+
 		return "redirect:/board/notice/main";
 	}
-	
+
 	@RequestMapping(value = "/board/notice/delete", method = RequestMethod.GET)
-	public String noticeDelete(HttpServletRequest request) {	
-		
+	public String noticeDelete(HttpServletRequest request) {
+
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("notice");
-		
+
 		boardService.deleteBoard(bb);
 
 		return "redirect:/board/notice/main";
 	}
-	
+
 //===============================================================================================
 //==========================================recipe===============================================	
-	
+
 	@RequestMapping(value = "/board/recipe/main", method = RequestMethod.GET)
-	public String recipeMain(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String recipeMain(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		String pageNum = request.getParameter("pageNum");
 		String category = "recipe";
 		PageBean pb = new PageBean();
 		pb.setPageSize(10);
-		if(pageNum == null) {
+		if (pageNum == null) {
 			pageNum = "1";
 		}
 		pb.setPageNum(pageNum);
 		pb.setCategory(category);
 		List<BoardBean> recipeList = boardService.getBoardList(pb);
-		
+
 		pb.setCount(boardService.getBoardCount(category));
-		
+
 		session.setAttribute("id", id);
 		model.addAttribute("recipeList", recipeList);
 		model.addAttribute("pb", pb);
-		
+
 		return "/board/recipe/main";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/write", method = RequestMethod.GET)
-	public String recipeWrite(HttpSession session) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String recipeWrite(HttpSession session) {
+
+		String id = (String) session.getAttribute("id");
 		session.setAttribute("id", id);
-		
+
 		return "/board/recipe/write";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/write", method = RequestMethod.POST)
-	public String recipeWritePost(BoardBean bb) {	
-		
+	public String recipeWritePost(BoardBean bb) {
+
 		bb.setCategory("recipe");
 		boardService.writeBoard(bb);
-		
+
 		return "redirect:/main";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/content", method = RequestMethod.GET)
-	public String recipeContent(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String recipeContent(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("recipe");
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/recipe/content";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/update", method = RequestMethod.GET)
-	public String recipeUpdate(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String recipeUpdate(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("recipe");
-		
+
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/recipe/update";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/update", method = RequestMethod.POST)
-	public String recipeUpdatePost(BoardBean bb) {	
-		
+	public String recipeUpdatePost(BoardBean bb) {
+
 		bb.setCategory("recipe");
 		boardService.updateBoard(bb);
-	
+
 		return "redirect:/board/recipe/main";
 	}
-	
+
 	@RequestMapping(value = "/board/recipe/delete", method = RequestMethod.GET)
-	public String recipeDelete(HttpServletRequest request) {	
-		
+	public String recipeDelete(HttpServletRequest request) {
+
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("recipe");
@@ -216,115 +216,127 @@ public class BoardController {
 
 		return "redirect:/board/recipe/main";
 	}
-	
+
 //===============================================================================================
 //==========================================picture===============================================	
-	
+
 	@RequestMapping(value = "/board/picture/main", method = RequestMethod.GET)
-	public String pictureMain(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String pictureMain(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		String pageNum = request.getParameter("pageNum");
 		String category = "picture";
 		PageBean pb = new PageBean();
 		pb.setPageSize(4);
-		if(pageNum == null) {
+		if (pageNum == null) {
 			pageNum = "1";
 		}
 		pb.setPageNum(pageNum);
 		pb.setCategory(category);
 		List<BoardBean> pictureList = boardService.getBoardList(pb);
-		
+
 		pb.setCount(boardService.getBoardCount(category));
-		
+
 		session.setAttribute("id", id);
 		model.addAttribute("pictureList", pictureList);
 		model.addAttribute("pb", pb);
-		
+
 		return "/board/picture/main";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/write", method = RequestMethod.GET)
-	public String pictureWrite(HttpSession session) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String pictureWrite(HttpSession session) {
+
+		String id = (String) session.getAttribute("id");
 		session.setAttribute("id", id);
-		
+
 		return "/board/picture/write";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/write", method = RequestMethod.POST)
-	public String pictureWritePost(HttpServletRequest request, MultipartFile file) throws IOException {	
+	public String pictureWritePost(HttpServletRequest request, MultipartFile file) throws IOException {
 //		System.out.println("@@@@@@@@@@@@@@@");
 //		System.out.println("파일이름 : " + file.getOriginalFilename());
 //		System.out.println("파일크기 : " + file.getSize());
 //		System.out.println("파일타입 : " + file.getContentType());
 		String category = "picture";
-		
+
 		UUID uid = UUID.randomUUID();
 		String saveName = uid.toString() + "_" + file.getOriginalFilename();
-		
-		File target = new File(uploadPath,saveName);
+
+		File target = new File(uploadPath, saveName);
 		FileCopyUtils.copy(file.getBytes(), target);
-		
+
 		BoardBean bb = new BoardBean();
 		bb.setCategory(category);
 		bb.setContent(request.getParameter("content"));
 		bb.setFile(saveName);
 		bb.setName(request.getParameter("name"));
 		bb.setSubject(request.getParameter("subject"));
-		
+
 		boardService.writeBoard(bb);
-		
+
 		return "redirect:/main";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/content", method = RequestMethod.GET)
-	public String pictureContent(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String pictureContent(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("picture");
-		
+
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/picture/content";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/update", method = RequestMethod.GET)
-	public String pictureUpdate(HttpSession session, HttpServletRequest request, Model model) {	
-		
-		String id = (String)session.getAttribute("id");
+	public String pictureUpdate(HttpSession session, HttpServletRequest request, Model model) {
+
+		String id = (String) session.getAttribute("id");
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("picture");
 		BoardBean bb2 = boardService.getBoard(bb);
-		
-		session.setAttribute("id", id);	
+
+		session.setAttribute("id", id);
 		model.addAttribute("bb", bb2);
 		return "/board/picture/update";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/update", method = RequestMethod.POST)
-	public String pictureUpdatePost(BoardBean bb) {	
-		
+	public String pictureUpdatePost(MultipartFile file, HttpServletRequest request) throws IOException {
+
 		String category = "picture";
+		UUID uid = UUID.randomUUID();
+		String saveName = uid.toString() + "_" + file.getOriginalFilename();
+
+		File target = new File(uploadPath, saveName);
+		FileCopyUtils.copy(file.getBytes(), target);
+
+		BoardBean bb = new BoardBean();
 		bb.setCategory(category);
+		bb.setNum(Integer.parseInt(request.getParameter("num")));
+		bb.setContent(request.getParameter("content"));
+		bb.setFile(saveName);
+		bb.setName(request.getParameter("name"));
+		bb.setSubject(request.getParameter("subject"));
 		boardService.updateBoard(bb);
-	
-		return "redirect:/board/recipe/main";
+
+		return "redirect:/board/picture/main";
 	}
-	
+
 	@RequestMapping(value = "/board/picture/delete", method = RequestMethod.GET)
-	public String delete(HttpServletRequest request) {	
-		
+	public String delete(HttpServletRequest request) {
+
 		BoardBean bb = new BoardBean();
 		bb.setNum(Integer.parseInt(request.getParameter("num")));
 		bb.setCategory("picture");
-		
+
 		boardService.deleteBoard(bb);
 
 		return "redirect:/board/picture/main";
--- FunWeb_Spring/src/main/webapp/WEB-INF/views/board/picture/content.jsp
+++ FunWeb_Spring/src/main/webapp/WEB-INF/views/board/picture/content.jsp
@@ -9,8 +9,6 @@
 <title>Insert title here</title>
 <link href='<c:url value="/resources/css/default.css" />' rel="stylesheet" type="text/css">
 <link href='<c:url value="/resources/css/subpage.css" />' rel="stylesheet" type="text/css">
-<script type="text/javascript">
-</script>
 </head>
 <body>
 <div id="wrap">
@@ -33,57 +31,40 @@
 <form method="post" enctype="multipart/form-data">
 <table border="1" id="notice">
 <tr><th></th><th></th><th></th><th></th><th></th><th></th></tr>
-	<tr><td>글번호</td><td><%=bb.getNum()%></td><td>조회수</td><td><%=bb.getReadcount()%></td><td>추천수</td><td><%=bb.getRecommend()%></td></tr>
-	<tr><td>글쓴이</td><td><%=bb.getName()%></td><td>작성일</td><td colspan="3"><%=bb.getDate()%></td></tr>
-	<tr><td>글제목</td><td colspan ="5"><%=bb.getSubject() %></td></tr>
-	<tr><td>글내용</td><td colspan="5"><%=bb.getContent()%></td></tr>
-	<tr><td>파일</td><td colspan="5"> <img src="../upload/<%=bb.getFile()%>" width="300" height="300"></td></tr>
+	<tr><td>글번호</td><td>${bb.num }</td><td>조회수</td><td>${bb.readcount }</td><td>추천수</td><td>${bb.recommend }</td></tr>
+	<tr><td>글쓴이</td><td>${bb.name }</td><td>작성일</td><td colspan="3">${bb.date }</td></tr>
+	<tr><td>글제목</td><td colspan ="5">${bb.subject }</td></tr>
+	<tr><td>글내용</td><td colspan="5">${bb.content }</td></tr>
+	<tr><td>파일</td><td colspan="5"> <img src='<c:url value="/resources/upload/${bb.file }"/>' width="300" height="300"></td></tr>
 	</table>
-	<button type="button" class="rbtn" onclick="location.href='imageRecommendPro.jsp?num=<%=bb.getNum()%>'"><img src="../images/따봉2.jpg" width="80px" height="80px" ></button>
+	<img alt="" src='<c:url value="/resources/images/따봉2.jpg"/>' class="rbtn" border="3" onclick='<c:url value="#" />'>
 <div id="table_search">
-<input type="button" value="글목록" onclick="location.href='imageBoardList.jsp'" class="btn">
-<%if(id.equals(bb.getName())){%>
-<input type="button" value="글수정" onclick="location.href='imageUpdateForm.jsp?num=<%=bb.getNum()%>'" class="btn">
-<input type="button" value="글삭제" onclick="location.href='imageDeletePro.jsp?num=<%=bb.getNum()%>'" class="btn">
-<%
-}
-%>
+<input type="button" value="글목록" onclick='location.href="<c:url value="/board/piture/main"/>"' class="btn">
+<c:if test="${sessionScope.id eq bb.name }">
+<input type="button" value="글수정" onclick='location.href="<c:url value="/board/picture/update?num=${bb.num }"/>"' class="btn">
+<input type="button" value="글삭제" onclick='location.href="<c:url value="/board/picture/delete?num=${bb.num }"/>"' class="btn">
+</c:if>
 </div>
 </form>
 <div class="clear"></div>
 <hr>
 <h2>Comment</h2>
 <form action="CommentPro.jsp" method="get">
-<input type= "hidden"  name="name" value="<%=id%>">
-<input type= "hidden"  name="ref" value="<%=bb.getNum()%>">
+<input type= "hidden"  name="name" value="${sessionScope.id }">
+<input type= "hidden"  name="ref" value="${bb.name }">
 <textarea rows="3" cols="50" name="content" ></textarea><input type="submit" value="댓글등록" class="btn">
 </form>
-<%
-for(int i = 0 ; i < commentList.size() ; i++){
-	CommentBean cb = commentList.get(i);
-%>
 <hr>
 <table>
-<tr><td><b><%=cb.getName()%></b></td></tr>
-<tr><td colspan="3"><%=cb.getContent()%></td></tr>
-<tr><td><%=cb.getDate()%></td></tr>
+<%-- <tr><td><b><%=cb.getName()%></b></td></tr> --%>
+<%-- <tr><td colspan="3"><%=cb.getContent()%></td></tr> --%>
+<%-- <tr><td><%=cb.getDate()%></td></tr> --%>
 </table>
-
-<%
-	}
-%>
 </article>
 
 <div class="clear"></div>
 <article>
 <div id="page_control">
-<a href="#">Prev</a>
-<%for (int i = startPage ; i <= endPage ; i++){ %>
-	<a href="imageContent.jsp?num=<%=ref%>&pageNum=<%=i%>"><%=i%></a>
-	<%
-}
-%>
-<a href="#">Next</a>
 </div>
 </article>
 
@@ -92,7 +73,7 @@ for(int i = 0 ; i < commentList.size() ; i++){
 <!-- 본문들어가는 곳 -->
 <div class="clear"></div>
 <!-- 푸터들어가는 곳 -->
-<jsp:include page="../inc/bottom.jsp"  />
+<jsp:include page="../../inc/bottom.jsp"  />
 <!-- 푸터들어가는 곳 -->
 </div>
 </body>
